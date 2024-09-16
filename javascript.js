2e646b5ef0e9c75f144806ecd82388f3c4131ed9
46tcampbell <157092336+46tcampbell@users.noreply.github.com>
2024-09-16 15:27:02 -0500

    Create logic for human choice
    This commit adds the javascript for the human
    choice by capturing a prompt input and returning
    a message to the user as well as recording the
    value.
    
    Psuedocode included at the top and code in the
    bottom.

--- javascript.js
+++ javascript.js
@@ -1,29 +1,43 @@
-    //Create a new function named getComputerChoice//
-        //function getComputerChoice() {
-        //}//
+//Step 2 Code Below//
 
-    //Code a random number//
-      //let randomNumber =  Math.floor(Math.random() * 100)//
-    //Assign return values to random selector (“rock”, “paper” or “scissors”)//
-        //if (randomNumber >= 0 && randomNumber <= 32) {
-            //return "rock";
-        //} else if (randomNumber >= 33 && randomNumber <= 65) {
-            //return "paper";
-        //} else {
-            //return "scissors"
-        //}//
-    //Print value selected//
+function getComputerChoice() {
+    let randomNumber =  Math.floor(Math.random() * 100);
 
-        function getComputerChoice() {
-            let randomNumber =  Math.floor(Math.random() * 100);
-
-                if (randomNumber >= 0 && randomNumber <= 32) {
-                    return "rock";
-                } else if (randomNumber >= 33 && randomNumber <= 65) {
-                    return "paper";
-                } else {
-                    return "scissors";
-                }
+        if (randomNumber >= 0 && randomNumber <= 32) {
+            return "rock";
+        } else if (randomNumber >= 33 && randomNumber <= 65) {
+            return "paper";
+        } else {
+            return "scissors";
         }
+}
 
-        console.log(getComputerChoice())
+console.log(getComputerChoice())
+
+//Step 3 Code Below//
+
+//Create a new function named getHumanChoice.
+//User inputs choice
+//choice is stored in variable
+//choice is printed//
+
+    function getHumanChoice() {
+        let humanChoice = prompt("Please enter \"rock\", \"paper\" or \"scissors\". Alternative inputs will default to \"rock\".");
+
+        if (humanChoice.toLowerCase() === "rock") {
+            alert("You have chosen rock");
+            return "rock";
+        } else if (humanChoice.toLowerCase() === "paper") {
+            alert("You have chosen paper");
+            return "paper";
+        } else if (humanChoice.toLowerCase() === "scissors") {
+            alert("You have chosen scissors");
+            return "scissors";
+        } else {
+            alert("Invalid Choice");
+            return "rock";
+        }
+    }
+
+    console.log(getHumanChoice())
+
