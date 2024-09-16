20695c4eda357f9594413fde37039f8885bc0b2c
46tcampbell <157092336+46tcampbell@users.noreply.github.com>
2024-09-16 14:48:03 -0500

    Create code logic for computer choice
    Final code at the bottom has been checked in the
    consolde and is producing rock, paper or scissors
    randomly so moving on.
    
    Psuedocode included and commented out in the top
    to keep track of progress.

--- javascript.js
+++ javascript.js
@@ -1,1 +1,29 @@
-console.log("Hello Word");
+    //Create a new function named getComputerChoice//
+        //function getComputerChoice() {
+        //}//
+
+    //Code a random number//
+      //let randomNumber =  Math.floor(Math.random() * 100)//
+    //Assign return values to random selector (“rock”, “paper” or “scissors”)//
+        //if (randomNumber >= 0 && randomNumber <= 32) {
+            //return "rock";
+        //} else if (randomNumber >= 33 && randomNumber <= 65) {
+            //return "paper";
+        //} else {
+            //return "scissors"
+        //}//
+    //Print value selected//
+
+        function getComputerChoice() {
+            let randomNumber =  Math.floor(Math.random() * 100);
+
+                if (randomNumber >= 0 && randomNumber <= 32) {
+                    return "rock";
+                } else if (randomNumber >= 33 && randomNumber <= 65) {
+                    return "paper";
+                } else {
+                    return "scissors";
+                }
+        }
+
+        console.log(getComputerChoice())
