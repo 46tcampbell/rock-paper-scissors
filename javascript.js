29683b99465db6a8d0fe741af4b600c7a1d5325d
46tcampbell <157092336+46tcampbell@users.noreply.github.com>
2025-05-27 07:26:16 -0500

    Save progress on Chromebook
    Marking save on 5/27/25 from yesterday's updates so i can continue in
    Addison

--- javascript.js
+++ javascript.js
@@ -1,5 +1,21 @@
 let humanScore = 0;
 let computerScore = 0;
+let draws = 0;
+const scoreDisplay = document.createElement('div')
+const humanScoreDisplay = document.createElement('span');
+humanScoreDisplay.textContent = `Human Score: ${humanScore}`;
+const computerScoreDisplay = document.createElement('span');
+computerScoreDisplay.textContent = ` Computer Score: ${computerScore}`;
+const drawsDisplay = document.createElement('span');
+drawsDisplay.textContent = `Draws: ${draws}`
+const ul = document.createElement('ul');
+scoreDisplay.appendChild(humanScoreDisplay);
+scoreDisplay.appendChild(computerScoreDisplay);
+scoreDisplay.appendChild(drawsDisplay);
+scoreDisplay.appendChild(ul);
+document.body.appendChild(scoreDisplay);
+
+
 
 function getComputerChoice() {
     let randomNumber = Math.floor(Math.random() * 100);
@@ -13,53 +29,114 @@ function getComputerChoice() {
     }
 }
 
-function getHumanChoice() {
-    let humanChoice = prompt("Please enter \"rock\", \"paper\" or \"scissors\". Alternative inputs will default to \"rock\".");
-
-    if (humanChoice.toLowerCase() === "rock") {
-        alert("You have chosen rock");
-    } else if (humanChoice.toLowerCase() === "paper") {
-        alert("You have chosen paper");
-    } else if (humanChoice.toLowerCase() === "scissors") {
-        alert("You have chosen scissors");
-    } else {
-        alert("Invalid Choice");
-    }
+function getHumanChoice(buttonChoice) {
+    let humanChoice = buttonChoice;
     return humanChoice.toLowerCase();
 }
 
+const rockButton = document.querySelector('#rock');
+rockButton.addEventListener('click', () => {
+    playRound(getHumanChoice('rock'), getComputerChoice());
+});
+const paperButton = document.querySelector('#paper');
+paperButton.addEventListener('click', () => {
+    playRound(getHumanChoice('paper'), getComputerChoice());
+});
+const scissorsButton = document.querySelector('#scissors');
+scissorsButton.addEventListener('click', () => {
+    playRound(getHumanChoice('scissors'), getComputerChoice());
+});
+
+
+let isGameOver = false;
+
 
 function playRound(humanChoice, computerChoice) {
-    if (humanChoice === "rock" && computerChoice === "paper") {
-        console.log("You lose this round! Paper beats Rock.");
-        return ++computerScore;
-    } else if (humanChoice === "rock" && computerChoice === "scissors") {
-        console.log("You win this round! Rock beats Scissors.");
-        return ++humanScore;
-    } else if (humanChoice === "paper" && computerChoice === "rock") {
-        console.log("You win this round! Paper beats Rock.");
-        return ++humanScore;
-    } else if (humanChoice === "paper" && computerChoice === "scissors") {
-        console.log("You lose this round! Scissors beats Paper.");
-        return ++computerScore;
-    } else if (humanChoice === "scissors" && computerChoice === "paper") {
-        console.log("You win this round! Scissors beats Paper.");
-        return ++humanScore;
-    } else if (humanChoice === "scissors" && computerChoice === "rock") {
-        console.log("You lose this round! Rock beats scissors.")
-        return ++computerScore;
+    if (!isGameOver) {
+        if (humanChoice === "rock" && computerChoice === "paper") {
+            const li = document.createElement('li');
+            li.textContent = "You have chosen rock. You lose this round! Paper beats Rock.";
+            ul.appendChild(li);
+            ++computerScore;
+            computerScoreDisplay.textContent = ` Computer Score: ${computerScore}`;
+            if (humanScore >= 5 || computerScore >= 5) {
+                isGameOver = true
+            }
+        } else if (humanChoice === "rock" && computerChoice === "scissors") {
+            const li = document.createElement('li');
+            li.textContent = "You have chosen rock. You win this round! Rock beats Scissors.";
+            ul.appendChild(li);
+            ++humanScore;
+            humanScoreDisplay.textContent = `Human Score: ${humanScore}`;
+            if (humanScore >= 5 || computerScore >= 5) {
+                isGameOver = true
+            }
+        } else if (humanChoice === "paper" && computerChoice === "rock") {
+            const li = document.createElement('li');
+            li.textContent = "You have chosen paper. You win this round! Paper beats Rock.";
+            ul.appendChild(li);
+            ++humanScore;
+            humanScoreDisplay.textContent = `Human Score: ${humanScore}`;
+            if (humanScore >= 5 || computerScore >= 5) {
+                isGameOver = true
+            }
+        } else if (humanChoice === "paper" && computerChoice === "scissors") {
+            const li = document.createElement('li');
+            li.textContent = "You have chosen paper. You lose this round! Scissors beats Paper.";
+            ul.appendChild(li);
+            ++computerScore;
+            computerScoreDisplay.textContent = ` Computer Score: ${computerScore}`;
+            if (humanScore >= 5 || computerScore >= 5) {
+                isGameOver = true
+            }
+        } else if (humanChoice === "scissors" && computerChoice === "paper") {
+            const li = document.createElement('li');
+            li.textContent = "You have chosen scissors. You win this round! Scissors beats Paper.";
+            ul.appendChild(li);
+            ++humanScore;
+            humanScoreDisplay.textContent = `Human Score: ${humanScore}`;
+            if (humanScore >= 5 || computerScore >= 5) {
+                isGameOver = true
+            }
+        } else if (humanChoice === "scissors" && computerChoice === "rock") {
+            const li = document.createElement('li');
+            li.textContent = "You have chosen scissors. You lose this round! Rock beats scissors.";
+            ul.appendChild(li);
+            ++computerScore;
+            computerScoreDisplay.textContent = ` Computer Score: ${computerScore}`;
+            if (humanScore >= 5 || computerScore >= 5) {
+                isGameOver = true
+            }
+        } else {
+            const li = document.createElement('li');
+            li.textContent = "DRAW! No points for anyone.";
+            ul.appendChild(li);
+            ++draws;
+            drawsDisplay.textContent = `Draws: ${draws}`
+            if (humanScore >= 5 || computerScore >= 5) {
+                isGameOver = true
+            } else {
+                isGameOver = false
+            }
+        }
     } else {
-        console.log("DRAW! No points for anyone.")
+        ul.remove();
+        humanScore = 0;
+        humanScoreDisplay.textContent = `Human Score: ${humanScore}`;
+        computerScore = 0;
+        computerScoreDisplay.textContent = ` Computer Score: ${computerScore}`;
+        draws = 0;
+        drawsDisplay.textContent = `Draws: ${draws}`
     }
 }
 
 function playGame() {
 
-    playRound(getHumanChoice(), getComputerChoice());
-    playRound(getHumanChoice(), getComputerChoice());
-    playRound(getHumanChoice(), getComputerChoice());
-    playRound(getHumanChoice(), getComputerChoice());
-    playRound(getHumanChoice(), getComputerChoice());
+    // playRound(getHumanChoice(), getComputerChoice());
+    // playRound(getHumanChoice(), getComputerChoice());
+    // playRound(getHumanChoice(), getComputerChoice());
+    // playRound(getHumanChoice(), getComputerChoice());
+    // playRound(getHumanChoice(), getComputerChoice());
 
     if (humanScore > computerScore) {
         console.log("You win the whole game!");
@@ -73,6 +150,6 @@ function playGame() {
     console.log("Computer Score = " + computerScore);
 }
 
-playGame()
+// playGame()
 
 
